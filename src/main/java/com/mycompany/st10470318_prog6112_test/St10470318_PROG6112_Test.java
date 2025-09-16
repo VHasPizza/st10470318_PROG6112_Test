b87125c26f053684cd861503e362f8aff3d8264b
VHasPizza <virnerusteberg@gmail.com>
2025-09-16 08:46:58 +0200

    created project
    I added the test to github, created a while loop and assed the questions for user input

---
+++ src/main/java/com/mycompany/st10470318_prog6112_test/St10470318_PROG6112_Test.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.st10470318_prog6112_test;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class St10470318_PROG6112_Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //user input for accident and location city
+        //display total accidents recorded
+        //must display vehial type 
+        boolean blnRun = true;
+        
+        while (blnRun = true){
+            
+            System.out.println("Enter the number of CAR accidents for Cape Town");
+            System.out.println("Enter the number of BIKE accidents for Cape Town");
+            System.out.println("Enter the number of CAR accidents for Jonah");
+            System.out.println("Enter the number of BIKE accidents for Cape Town");
+            System.out.println("Enter the number of CAR accidents for Cape Town");
+            System.out.println("Enter the number of BIKE accidents for Cape Town");
+            
+            
+            
+        }
+            
+        
+    }
+}
